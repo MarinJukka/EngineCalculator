f3d5c19ad2126e14967df572e1b469f773598305
MarinJukka <49443465+MarinJukka@users.noreply.github.com>
2020-02-11 14:52:19 +0100

    Update PistonAreaTest.java

--- EngineCalculator/src/tests/PistonAreaTest.java
+++ EngineCalculator/src/tests/PistonAreaTest.java
@@ -4,6 +4,7 @@ import com.company.PistonArea;
 import org.junit.Before;
 
 public class PistonAreaTest {
+    //In dem Bevor erzeugt du die Objekte, die spaeter bentutzt werden zum testen, also yb PistonArea a = new PistonArea(...)
     @Before
     public void setUp() throws Exception{
         new PistonArea(0.0,0.0);
